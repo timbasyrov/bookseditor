1df1216169e3553475a66598c5591cb4f2b61c90
Timur Basyrov <t.basyrov@inostudio.com>
2016-09-09 22:38:28 +0500

    Add route config for ractive app

---
+++ BooksEditor/Scripts/app/components/Router.js
@@ -0,0 +1,17 @@
+﻿// Source: http://paquitosoftware.com/ractive-js-tutorial-routing/
+Ractive.components.Router = Ractive.extend({
+    template: '<router-handler params="{{params}}"/>',
+    components: {
+        'router-handler': function () {
+            return this.get('componentName');
+        }
+    },
+    oninit: function () {
+        var it = this;
+        it.observe('componentName', function (newValue, oldValue) {
+            if (this.fragment.rendered) {
+                this.reset();
+            }
+        });
+    },
+});
---
+++ BooksEditor/Scripts/routes/route-configuration.js
@@ -0,0 +1,44 @@
+﻿var RouteConfiguration = function ()
+{
+};
+
+RouteConfiguration.prototype = {
+    _routes: [],
+
+    AddRoute: function (route, componentName, isDefault) {
+        this._routes.push({ route: route, componentName: componentName, isDefault: isDefault });
+    },
+
+    Register: function (app) {
+        var it = this;
+
+        //set hashband as root for client routing; that means, that reload page won't cause 404
+        page.base('/#!');
+        
+        //finding default route, if multiple routes were marked as default, we take the first one.
+        var defaultRoute = _.find(it._routes, function (route) {
+            return route.isDefault;
+        });
+
+        //if no one was marked as default we take the first added route.
+        if (!defaultRoute) {
+            defaultRoute = it._routes[0];
+        };
+
+        if (defaultRoute) {
+            page('/', defaultRoute.route);
+        }
+        
+        _.each(it._routes, function (route) {
+            page(route.route, function(context, next){
+                context.componentName = route.componentName;
+                app.onNavigation(null, context);
+            });
+        });
+
+        //set all other routes *
+        page();
+
+        page({ hashbang: true });
+    },
+};
