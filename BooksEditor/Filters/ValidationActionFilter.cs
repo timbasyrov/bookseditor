6f934b2b1d2a32a4b5a3acc3b4fb144ee32be966
Timur Basyrov <t.basyrov@inostudio.com>
2016-09-09 22:30:16 +0500

    Add validation filter

---
+++ BooksEditor/Filters/ValidationActionFilter.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using BooksEditor.Services.Models;
+
+namespace BooksEditor.Filters
+{
+    public class ValidationActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext context)
+        {
+            var modelState = context.ModelState;
+            if (!modelState.IsValid)
+            {
+                ActionResultModel actionResult = new ActionResultModel();
+                actionResult.IsSuccess = false;
+
+                foreach (var key in modelState.Keys)
+                {
+                    var state = modelState[key];
+                    if (state.Errors.Any())
+                    {
+                        actionResult.Errors.Add(state.Errors.First().ErrorMessage);
+                    }
+                }
+
+                context.Response = context.Request.CreateResponse(actionResult);
+            }
+        }
+    }
+}
