1b581f5eca0efb80b1e1d9b47b2f7bbf0eb5abc6
Timur Basyrov <t.basyrov@inostudio.com>
2016-09-09 22:29:09 +0500

    Add author controller

---
+++ BooksEditor/Controllers/AuthorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using BooksEditor.Data.Models;
+using BooksEditor.Services;
+using BooksEditor.Services.Models;
+
+namespace BooksEditor.Controllers
+{
+    public class AuthorController : ApiController
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Author> GetAuthorList([FromUri]AuthorListRequest request)
+        {
+            return _authorService.GetAuthorList(request);
+        }
+
+        [HttpGet]
+        public Author GetAuthor(int id)
+        {
+            return _authorService.GetAuthor(id);
+        }
+
+        [HttpPost]
+        public ActionResultModel SaveAuthor([FromBody]Author author)
+        {
+            return _authorService.SaveAuthor(author);
+        }
+
+        [HttpDelete]
+        public ActionResultModel DeleteAuthor(int id)
+        {
+            return _authorService.DeleteAuthor(id);
+        }
+    }
+}
