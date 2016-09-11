a9d63476e32ad9ec477c02cb20a9bfa67edb4796
Timur Basyrov <t.basyrov@inostudio.com>
2016-09-12 00:37:20 +0500

    Fix publication year validation

---
+++ BooksEditor.Services/Attributes/PublicationYearValidationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksEditor.Services.Attributes
+{
+    public class PublicationYearValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                int publicationYear = (int)value;
+                // Sometimes publication of book planned in near future
+                int maxPublicationYearAllowed = DateTime.Now.Year + 3;
+
+                if (publicationYear < 1800 || publicationYear > maxPublicationYearAllowed)
+                {
+                    return new ValidationResult(string.Format("Publication year must be in range from 1800 to {0}", maxPublicationYearAllowed));
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- BooksEditor.Services/Models/BookModel.cs
+++ BooksEditor.Services/Models/BookModel.cs
@@ -14,13 +14,13 @@ namespace BooksEditor.Services.Models
         public int[] Authors { get; set; }
 
         [Required]
-        [Range(0, 10000, ErrorMessage = "Page count must be in range from 0 to 10000")]
+        [Range(1, 10000, ErrorMessage = "Page count must be in range from 1 to 10000")]
         public int PageCount { get; set; }
 
         [MaxLength(30, ErrorMessage = "Name of the publishing house must be less 30 symbols")]
         public string PublishingHouse { get; set; }
 
-        [Range(1800, 2100, ErrorMessage = "Publication year must be in range from 1800 to 2100")]
+        [PublicationYearValidation]
         public int PublicationYear { get; set; }
 
         [ISBNValidation]
