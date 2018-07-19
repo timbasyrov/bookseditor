b76fa5b5c83b9159fe24514669bf52a4fd48f3ec
Timur Basyrov <timmthetiger@mail.ru>
2018-07-19 13:02:56 +0500

    Add hosting configuration file

--- BooksEditor/Program.cs
+++ BooksEditor/Program.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace BooksEditor
 {
@@ -14,7 +9,15 @@ namespace BooksEditor
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
+            CreateWebHostBuilder(args)
+                .UseConfiguration(config)
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
